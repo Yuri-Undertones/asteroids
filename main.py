2da12c27f0324e8a388f855580d3120b0f0e886e
Bakyn <astrod@live.com>
2025-04-09 14:30:01 -0400

    create rotation

--- main.py
+++ main.py
@@ -5,22 +5,24 @@ from player import *
 
 def main():
     pygame.init()
+    running = True
     clock = pygame.time.Clock()
     dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    while True:
+    
+    while running:
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
-        
     
 
 
