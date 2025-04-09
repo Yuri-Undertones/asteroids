4dbdf12aa4bec73cea302cd5bcfb2a64fc8b5e34
Yuri-Undertones <andrue.hackshaw@gmail.com>
2025-04-09 12:02:45 -0400

    test3

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import pygame
-import sys
 
 def main():
-    print("Starting Asteroids!", flush=True)
-    sys.stdout.flush()  # Extra flush just to be sure
+    print("Starting Asteroids!")
 
 if __name__ == "__main__":
     main()
-
+    
