b8b1caecd77cc668e49d36bc3c3e30e8b853e8b7
Yuri-Undertones <andrue.hackshaw@gmail.com>
2025-04-09 11:53:48 -0400

    test

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import pygame
+
+print("Outside main function")
+
+def main():
+    print("DEBUG: Before message")
+    print("Starting Asteroids!")
+    print("DEBUG: After message")
+
+if __name__ == "__main__":
+    print("Running main")
+    main()
